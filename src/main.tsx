b3dad4cf08a41df30b317a19172fc65e0dcf1749
jose Angel <josegallardo.cor@gmail.com>
2025-10-17 18:09:09 -0600

    feat: componente de contactos pero con tema en el elemento toast

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Hero } from './components/hero.tsx'
 import { About } from './components/about.tsx'
 import { Skills } from './components/skills.tsx'
 import { Projects } from './components/projects.tsx'
+import { Contact } from './components/Contact.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,5 +16,6 @@ createRoot(document.getElementById('root')!).render(
     <About/>
     <Skills/>
     <Projects/>
+    <Contact/>
   </StrictMode>,
 )
